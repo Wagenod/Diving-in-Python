940a4d200c757f8f256bb34307aadc3eb3e9069a
unknown <wagenod93@mail.ru>
2020-03-15 23:23:16 +0300

    Started write lesson on modules and packages

---
+++ Week 1/Конспекты/module.py
@@ -0,0 +1,12 @@
+   #module.py
+    
+print("Importing module.py")
+   
+def foo():
+    print("Function foo")
+   
+print("Function foo is declared")
+foo()
+    
+if __name__ == '__main__':
+    print("In main part")
